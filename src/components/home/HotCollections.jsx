d71fe9a599414c3c4544a5459ba075623c549aa3
muh-dixon <shabil.dixon@gmail.com>
2026-01-30 00:21:33 -0500

    Hot Collections Draft,  needs review

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <button
+      className="carousel-arrow prev"
+      onClick={onClick}
+      aria-label="Previous"
+    >
+      ‹
+    </button>
+  );
+};
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <button
+      className="carousel-arrow next"
+      onClick={onClick}
+      aria-label="Next"
+    >
+      ›
+    </button>
+  );
+};
+
 
 const HotCollections = () => {
+  const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCollections() {
+      try {
+        const res = await fetch(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        const data = await res.json();
+
+        // Limit to 4 users
+        setCollections(data);
+      } catch (err) {
+        console.error("Failed to fetch collections", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCollections();
+  }, []);
+
+  const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: <PrevArrow />,
+  nextArrow: <NextArrow />,
+  responsive: [
+    { breakpoint: 1200, settings: { slidesToShow: 3 } },
+    { breakpoint: 992, settings: { slidesToShow: 2 } },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        arrows: false, // touch-friendly
+      },
+    },
+  ],
+};
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,30 +85,50 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
+        </div>
+
+        {loading ? (
+          <p className="text-center">Loading collections...</p>
+        ) : (
+          <Slider {...sliderSettings}>
+            {collections.map((item) => (
+              <div key={item.id}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={item.nftImage}
+                        className="img-fluid"
+                        alt={item.title}
+                      />
+                    </Link>
+                  </div>
+
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="pp-coll"
+                        src={
+                          item.authorImage ||
+                          "/images/author_thumbnail.jpg"
+                        }
+                        alt={item.authorName}
+                      />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <span>{item.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </Slider>
+        )}
       </div>
     </section>
   );
